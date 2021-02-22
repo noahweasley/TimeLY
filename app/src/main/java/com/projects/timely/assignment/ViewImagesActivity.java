92738e1f94f20e099d3827c4858275200ba963c5
Noah <iebenmelu@gmail.com>
2021-02-22 15:22:11 -0800

    Useless comment

--- app/src/main/java/com/projects/timely/assignment/ViewImagesActivity.java
+++ app/src/main/java/com/projects/timely/assignment/ViewImagesActivity.java
@@ -178,6 +178,7 @@ public class ViewImagesActivity extends AppCompatActivity implements ActionMode.
         indeterminateProgress2.setVisibility(View.VISIBLE);
         String[] uriList = intent.getStringArrayExtra(ARG_URI_LIST);
 
+        // Commenting
         SchoolDatabase database = new SchoolDatabase(this);
         ThreadUtils.runBackgroundTask(() -> {
             boolean isUpdated /* use the intent that started this activity */
