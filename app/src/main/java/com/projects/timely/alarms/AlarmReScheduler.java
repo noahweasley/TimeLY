fbe0992ae616c12e9508a304b6a846ac50d2fa9a
Noah <iebenmelu@gmail.com>
2021-06-08 23:40:26 -0700

    added alarm re-scheduler

---
+++ app/src/main/java/com/projects/timely/alarms/AlarmReScheduler.java
@@ -0,0 +1,34 @@
+package com.projects.timely.alarms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.projects.timely.core.SchoolDatabase;
+
+/**
+ * <p>An alarm re-scheduler that re-schedules alarm from TimeLY's database.</p>
+ * <p>
+ * When user devices switches off, all alarms are terminated and no longer exists.
+ * On re-boot, this receiver would re-set the alarms that was previously set.
+ */
+public class AlarmReScheduler extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        SchoolDatabase database = new SchoolDatabase(context);
+
+        String action = intent.getAction();
+        boolean isValidBootAction
+                = /* Normal device boot */ action.equals("android.intent.action.BOOT_COMPLETED")
+                || /* on TimeLY upgrade */ action.equals("android.intent.action.PACKAGE_REPLACED")
+                || /* Device locked */ action.equals("android.intent.action.LOCKED_BOOT_COMPLETED")
+                || /* quick boot */ action.equals("android.intent.action.QUICKBOOT_POWERON")
+                || /* htc devices */ action.equals("com.htc.intent.action.QUICKBOOT_POWERON")
+                || /* debugging mode */ action.equals("com.projects.timely.alarm.testing");
+
+        if (isValidBootAction) {
+            context.startService(new Intent(context, AlarmReSchedulerService.class));
+        }
+    }
+}
