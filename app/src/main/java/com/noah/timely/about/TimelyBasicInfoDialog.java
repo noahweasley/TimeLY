7e945ca292452bddffb1b91a96dbdb326496952b
Noah <iebenmelu@gmail.com>
2021-06-21 20:15:56 -0700

    finished building TimelyBasicInfoDialog

--- app/src/main/java/com/noah/timely/about/TimelyBasicInfoDialog.java
+++ app/src/main/java/com/noah/timely/about/TimelyBasicInfoDialog.java
@@ -2,8 +2,13 @@ package com.noah.timely.about;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,14 +16,21 @@ import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 
+import com.noah.timely.BuildConfig;
 import com.noah.timely.R;
 
-public class TimelyBasicInfoDialog extends DialogFragment {
+public class TimelyBasicInfoDialog extends DialogFragment implements View.OnClickListener {
     public static final String TAG = "com.noah.timely.about.TimelyInfoDialog";
 
     public void show(Context context) {
         FragmentManager manager = ((FragmentActivity) context).getSupportFragmentManager();
         show(manager, TAG);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.close)
+            dismiss();
     }
 
     @NonNull
@@ -27,7 +39,7 @@ public class TimelyBasicInfoDialog extends DialogFragment {
         return new InfoDialog(getContext());
     }
 
-    private static class InfoDialog extends Dialog {
+    private class InfoDialog extends Dialog {
 
         public InfoDialog(@NonNull Context context) {
             super(context, R.style.Dialog_Closeable);
@@ -37,8 +49,23 @@ public class TimelyBasicInfoDialog extends DialogFragment {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-            getWindow().setBackgroundDrawableResource(R.drawable.bg_rounded_edges);
+            getWindow().setBackgroundDrawableResource(R.drawable.bg_rounded_edges_8);
             setContentView(R.layout.dialog_about);
+
+            ImageButton btn_close = findViewById(R.id.close);
+            btn_close.setOnClickListener(TimelyBasicInfoDialog.this);
+
+            TextView tv_version = findViewById(R.id.version);
+
+            String version = BuildConfig.VERSION_NAME;
+            String packageName = "com.noah.timely";
+
+            try {
+                PackageInfo packageInfo = getContext().getPackageManager().getPackageInfo(packageName, 0);
+                version = packageInfo.versionName;
+            } catch (PackageManager.NameNotFoundException ignored) {}
+
+            tv_version.setText(String.format("V%s", version));
         }
     }
 }
