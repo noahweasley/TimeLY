9ba1fc2df6eabdd1521a009240b32556031f5fca
Noah <iebenmelu@gmail.com>
2021-10-11 05:37:55 -0700

    chore: updated tools

---
+++ lib/src/main/java/com/tools/lib/FunctionalityTest.java
@@ -0,0 +1,12 @@
+package com.tools.lib;
+
+public class FunctionalityTest {
+
+   private static boolean isEmpty(CharSequence se) {
+      return se.equals("") || se == null;
+   }
+
+   public static void main(String... args) {
+
+   }
+}
