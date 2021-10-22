1bfae7ec31bca3daf6a7cf0db956bcb3b824de10
Noah <iebenmelu@gmail.com>
2021-10-22 11:51:19 -0700

    updated code

--- app/src/main/java/com/noah/timely/util/PatternUtils.java
+++ app/src/main/java/com/noah/timely/util/PatternUtils.java
@@ -26,12 +26,12 @@ public class PatternUtils {
    /**
     * Pattern to match 24 hours clock
     */
-   public static final String DATE_SHORT_24_HoursClock = "^[A-z]{3} \\d{2}, (?:[01][0-9]|2[0-3]):[0-5][0-9]$";
+   public static final String DATE_SHORT_24_HoursClock = "[A-z]{3} \\d{2}, (?:[01][0-9]|2[0-3]):[0-5][0-9]";
 
    /**
     * Pattern to match 24 hours clock
     */
-   public static final String DATE_SHORT_12_HoursClock = "^[A-z]{3} \\d{2}, (?:0?[0-9]|1[0-2]):[0-5][0-9] (?i)[ap]m$";
+   public static final String DATE_SHORT_12_HoursClock = "[A-z]{3} \\d{2}, (?:0?[0-9]|1[0-2]):[0-5][0-9] (?i)[ap]m";
 
    /**
     * Pattern to match all reasonable dates in format (dd-mm-yyyy or dd/mm/yyyy or dd.mm.yyyy or dd_mm_yyyy)
@@ -39,12 +39,17 @@ public class PatternUtils {
     *
     * @see java.text.SimpleDateFormat
     */
-   public static final String DATE_ALL = "^(?:(3[01]|[12][0-9]|0[1-9])[/._-](1[0-2]|0[1-9]))[/._-][0-9]{4}$";
+   public static final String DATE_ALL = "(?:(3[01]|[12][0-9]|0[1-9])[/._-](1[0-2]|0[1-9]))[/._-][0-9]{4}";
 
    /**
     * Pattern to match short dates in format, <code>MMM dd</code> for example, May 16
     */
    public static final String DATE_SHORT = "[A-z]{3} \\d{2}";
+
+   /**
+    * Pattern to match both 12 and 24 hours clock with short date in front, e.g May 6, 00:00 or May 6, 12:00 am
+    */
+   public static final String DATE_SHORT_12_24_HoursClock = DATE_SHORT + ", " + TIME_ALL;
 
    /**
     * Validates a regex pattern against an input
